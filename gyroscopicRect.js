d731ad208db889da311b0fac6cd66a9271478d80
MuditKhare0612 <ransam.bkkhare@gmail.com>
2025-12-27 19:10:17 +0530

    Update text to display acceleration values

--- gyroscopicRect.js
+++ gyroscopicRect.js
@@ -6,7 +6,8 @@ function draw() {
     background(0);
     fill(255);
     textSize(32);
-    text(`X: ${rotationX}`, width / 2, height / 4);
-    text(`Y: ${rotationY}`, width / 2, height / 2);
-    text(`Z: ${rotationZ}`, width / 2, 3 * height / 4);
+    text(`X: ${accelerationX}`, width / 2, height / 4);
+    text(`Y: ${accelerationY}`, width / 2, height / 2);
+    text(`Z: ${accelerationZ}`, width / 2, 3 * height / 4);
 }
+
